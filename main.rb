b2548d2fa73c2448cc530bbaee06aa463bd6adcc
bevieboo <bevin.sng@gmail.com>
2016-04-20 18:36:10 +1000

    split lines of ingredients and save to ingredients table

--- main.rb
+++ main.rb
@@ -22,6 +22,7 @@ helpers do
     !!current_user
     # Will return true if there is a user logged in.
   end
+
 end
 
 after do
@@ -57,6 +58,7 @@ delete '/login' do
 end
 
 get '/signup' do
+  @user = User.new
   erb :signup
 end
 
@@ -65,19 +67,31 @@ post '/signup' do
   user.first_name = params[:first_name]
   user.last_name = params[:last_name]
   user.img_url = params[:image]
-  if params[:password] == params[:password_confirm]
-    user.password = params[:password]
-  else
-    flash.now[:error2] = "Oops, that’s not the same password as the first one."
-  end
-  if !User.exists?(email: params[:email])
-    user.email = params[:email]
-    user.save
+  user.password = params[:password]
+  user.email = params[:email]
+  # if params[:password] == params[:password_confirm]
+  #   user.password = params[:password]
+  # else
+  #   flash.now[:error2] = "Oops, that’s not the same password as the first one."
+  # end
+  # if !User.exists?(email: params[:email])
+  #   user.email = params[:email]
+  #   if user.save
+  #     redirect to '/'
+  #   else
+  #     raise 'sdfsdf'
+  #     @user = user
+  #     erb :signup
+  #   end
+  # else
+  #   flash.now[:error] = "Email already exists."
+  # end
+  if user.save
     redirect to '/'
   else
-    flash.now[:error] = "Email already exists."
+    @user = user
+    erb :signup
   end
-  erb :signup
 end
 
 get '/recipes/new' do
@@ -97,23 +111,44 @@ post '/recipes' do
   # raise params.inspect
   recipe = Recipe.new
   recipe.name = params[:recipe_name]
+  recipe.img_url = params[:image]
   recipe.prep_time = params[:prep_time]
   recipe.cook_time = params[:cook_time]
   recipe.servings = params[:servings]
-  if !Ingredient.exists?(name: params[:ingredients_name])
-    new_ingredient = Ingredient.create name: params[:ingredients_name]
-    recipe.ingredients << new_ingredient
+
+  if params[:ingredients_name].include?("\r\n")
+    arr = params[:ingredients_name].split("\r\n")
+    arr.each do |ingredient|
+      if !Ingredient.exists?(name: ingredient)
+        new_ingredient = Ingredient.create name: ingredient
+        recipe.ingredients << new_ingredient
+      else
+        add_ingredient = Ingredient.find_by(name: ingredient)
+        recipe.ingredients << add_ingredient
+      end
+    end
+
   else
-    add_ingredient = Ingredient.find_by(name: params[:ingredients_name])
-    recipe.ingredients << add_ingredient
+    if !Ingredient.exists?(name: params[:ingredients_name])
+      new_ingredient = Ingredient.create name: params[:ingredients_name]
+      recipe.ingredients << new_ingredient
+    else
+      add_ingredient = Ingredient.find_by(name: params[:ingredients_name])
+      recipe.ingredients << add_ingredient
+    end
   end
+
   recipe.directions = params[:directions]
   recipe.user_id = session[:user_id]
   recipe.save
   redirect to '/recipes'
+
 end
 
 get '/results' do
+  @ingredients = Ingredient.where(name: params[:search_box])
+  @ingredients_id = @ingredients.first.id
+  @results = IngredientRecipe.where(ingredient_id: @ingredients_id)
   erb :results
 end
 
--- models/user.rb
+++ models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   has_many :recipes
   has_secure_password
   mount_uploader :img_url, ImageUploader
+  # validates :password, confirmation: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
